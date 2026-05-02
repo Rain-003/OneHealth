d9f9d2da3c3b59a2793f032250eeb5f413787290
Rain-003 <wrain.m22@gmail.com>
2026-05-02 18:01:05 +0800

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -17,14 +16,9 @@ export default defineConfig({
         // wayfinder({
         //     formVariants: true,
         // }),
+        
     ],
     esbuild: {
         jsx: 'automatic',
-    },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'resources/js'),
-            '/public/build/assets': path.resolve(__dirname, 'resources/js/assets'),
-        },
     },
 });
